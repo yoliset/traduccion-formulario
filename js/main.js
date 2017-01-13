171ac2b7964bab0b72a99a1a3b37b009dcf8dbdf
yolisett <lizbeth9589@gmail.com>
2017-01-13 12:57:45 -0500

    Terminado

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementsByName('email_input')[0].placeholder='Por favor ingrese su email';
+document.getElementsByName('password_input')[0].placeholder='Por favor ingrese su contraseña';
+titulo.innerText = "Por favor inicia sesión";
+recordar.innerText = "Recordar datos";
+boton.innerText = "Iniciar sesión";
